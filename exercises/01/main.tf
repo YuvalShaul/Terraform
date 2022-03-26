68e32a71fde9dc5c840fd2d0d7e3e5817506ad6b
Yuval Shaul <yuval.shaul@gmail.com>
2022-03-26 11:26:59 -0400

    moving to new format

--- exercises/01/main.tf
+++ exercises/01/main.tf
@@ -3,9 +3,21 @@
 # Declare the provider being used, in this case it's AWS.
 # This provider supports setting the provider version, AWS credentials as well as the region.
 # It can also pull credentials and the region to use from environment variables, which we have set, so we'll use those
-provider "aws" {
-  version = "~> 2.0"
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
 }
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
 
 # declare a resource stanza so we can create something, in this case a key pair
 resource "aws_key_pair" "my_key_pair" {
