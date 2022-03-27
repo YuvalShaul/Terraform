064013796ebff1d487b57dcf5b3a50cff07fcd20
Yuval Shaul <yuval.shaul@gmail.com>
2022-03-27 04:14:25 -0400

    updating main.tf

--- examples/providers/main.tf
+++ examples/providers/main.tf
@@ -1,9 +1,13 @@
-provider "aws" {
-  alias  = "west"
-  region = "us-west-2"
-}
 
-# provider "google" {
-#   project = "acme-app"
-#   region  = "us-central1"
-# }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 3.0"
+    }
+  }
+}
