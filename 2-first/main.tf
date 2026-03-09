c540a2e3f189a641697c205f4a526d03421ac093
Yuval Shaul <yuval.shaul@gmail.com>
2026-03-09 14:47:44 +0200

    adding link

--- 2-first/main.tf
+++ 2-first/main.tf
@@ -20,6 +20,10 @@ provider "aws" {
 
 
 # declare a resource stanza so we can create something, in this case a key pair
+# The aws_key_pair resource is documented here:
+#   https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/key_pair
+#
+
 resource "aws_key_pair" "my_key_pair" {
   key_name   = "${var.keyname}KP"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC+C+/k6T/7wy7+InYK6qwwSk87hneBpgJtlYEL2B6FB7X5+KOI2OVr6i4wPekXweRMaOOTqF0L68390pErSqKQbYOWUNNymed03csqvtI3x+H1N+ZrN+ajL5C85eh+sSo8mvGMANThue/SuYRe25lAUJWqbK1Sjs0TWBo2KaULx7lTLPFPdj8J03piXt2foZHGNAlo7bfVuVt+EcHTZQCsRKo3eOkhJinXz+7Ol4He+RnPzG2Hb0ZGbJXudSsRfIhUNTgTNlhPxTiHsZc55RPjHsI2zrkVqGno9cXZHo9B1HGuXjcTyyEPW5XGfvOP1gCm4uLu3HLvt9z8ACi4J2vJ osboxes@osboxes"
